10e5912b409a93561ea6cbd5d1119fe8d3b7f12c
Andrew Johnston <apjohnsto@gmail.com>
2021-02-04 20:21:09 +0000

    firecracker snapshot loading

--- kvm/firectl.js
+++ kvm/firectl.js
@@ -1,68 +1,69 @@
 const { connect } = require('tools/fire.js')
-const process = require('process')
 const { fs } = just.library('fs')
+const { sys } = just.library('sys')
 
-const { launch, watch } = process
 const { unlink } = fs
+const { fork, waitpid, exec } = sys
+const { SystemError } = just
 
 const config = require('vmconfig.json')
-just.print(JSON.stringify(config))
+
+function launch (program, ...args) {
+  const child = fork()
+  if (child === 0) {
+    const r = exec(program, args)
+    throw new SystemError(`exec ${r}`)
+  } else if (child < 0) {
+    throw new SystemError(`fork ${child}`)
+  }
+  return child
+}
 
 async function main () {
   const sockName = 'busy.sock'
   unlink(sockName)
-  const process = launch('assets/firecracker', ['--api-sock', sockName])
+  launch('assets/firecracker', '--api-sock', sockName)
   let fire
   while (!fire) {
     try {
       fire = await connect(sockName)
     } catch (err) {
-      just.print(err.message)
+      just.sys.usleep(100000)
     }
   }
   let res
-
+  if (just.args[2] === 'snapshot') {
+    const snapshot = {
+      snapshot_path: './snapshot_file',
+      mem_file_path: './mem_file',
+      enable_diff_snapshots: false,
+      resume_vm: true
+    }
+    res = await fire.put('/snapshot/load', snapshot)
+    if (res.statusCode !== 204) throw new Error('Could not load snapshot')
+    return
+  }
   res = await fire.put('/drives/rootfs', config.drives[0])
-  just.print(JSON.stringify(res, null, '  '))
+  if (res.statusCode !== 204) throw new Error('Could not add drive')
   res = await fire.put('/machine-config', config['machine-config'])
-  just.print(JSON.stringify(res, null, '  '))
+  if (res.statusCode !== 204) throw new Error('Could not machine config')
   res = await fire.put('/boot-source', config['boot-source'])
-  just.print(JSON.stringify(res, null, '  '))
+  if (res.statusCode !== 204) throw new Error('Could not boot source')
   res = await fire.put('/actions', { action_type: 'InstanceStart' })
-  just.print(JSON.stringify(res, null, '  '))
-
-  process.onStdout = (buf, bytes) => {
-    just.net.write(just.sys.STDOUT_FILENO, buf, bytes)
-  }
-  process.onStderr = (buf, bytes) => {
-    just.net.write(just.sys.STDERR_FILENO, buf, bytes)
-  }
-  const status = await watch(process)
-  just.print(status)
-/*
-  const snapshot = {
-    snapshot_path: './snapshot_file',
-    mem_file_path: './mem_file',
-    enable_diff_snapshots: false,
-    resume_vm: true
-  }
-  res = await fire.put('/snapshot/load', snapshot)
-  just.print(JSON.stringify(res, null, '  '))
-  res = await fire.patch('/vm', { state: 'Paused' })
-  just.print(JSON.stringify(res, null, '  '))
-
-  const config = {
-    snapshot_type: 'Full',
-    snapshot_path: './snapshot_file2',
-    mem_file_path: './mem_file2',
-    version: '0.23.0'
-  }
-  res = await fire.put('/snapshot/create', config)
-  just.print(JSON.stringify(res, null, '  '))
-
-  res = await fire.patch('/vm', { state: 'Resumed' })
-  just.print(JSON.stringify(res, null, '  '))
-*/
+  if (res.statusCode !== 204) throw new Error('Could not start micro vm')
+  just.setTimeout(async () => {
+    res = await fire.patch('/vm', { state: 'Paused' })
+    if (res.statusCode !== 204) throw new Error('Could not pause vm')
+    res = await fire.put('/snapshot/create', {
+      snapshot_type: 'Full',
+      snapshot_path: './snapshot_file',
+      mem_file_path: './mem_file',
+      version: '0.23.0'
+    })
+    if (res.statusCode !== 204) throw new Error('Could not create snapshot')
+    res = await fire.patch('/vm', { state: 'Resumed' })
+    if (res.statusCode !== 204) throw new Error('Could not resume vm')
+  }, 1000)
 }
 
 main().catch(err => just.error(err.stack))
