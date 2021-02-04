aa5689bdb147963e6fdf1a844f5cf7b9698adaff
Andrew Johnston <apjohnsto@gmail.com>
2021-02-04 19:18:29 +0000

    firecracker api

--- kvm/firectl.js
+++ kvm/firectl.js
@@ -1,16 +1,53 @@
 const { connect } = require('tools/fire.js')
+const process = require('process')
+const { fs } = just.library('fs')
 
-async function main (sockName = './busy.sock') {
-  const fire = await connect(sockName)
+const { launch, watch } = process
+const { unlink } = fs
+
+const config = require('vmconfig.json')
+just.print(JSON.stringify(config))
+
+async function main () {
+  const sockName = 'busy.sock'
+  unlink(sockName)
+  const process = launch('assets/firecracker', ['--api-sock', sockName])
+  let fire
+  while (!fire) {
+    try {
+      fire = await connect(sockName)
+    } catch (err) {
+      just.print(err.message)
+    }
+  }
+  let res
+
+  res = await fire.put('/drives/rootfs', config.drives[0])
+  just.print(JSON.stringify(res, null, '  '))
+  res = await fire.put('/machine-config', config['machine-config'])
+  just.print(JSON.stringify(res, null, '  '))
+  res = await fire.put('/boot-source', config['boot-source'])
+  just.print(JSON.stringify(res, null, '  '))
+  res = await fire.put('/actions', { action_type: 'InstanceStart' })
+  just.print(JSON.stringify(res, null, '  '))
+
+  process.onStdout = (buf, bytes) => {
+    just.net.write(just.sys.STDOUT_FILENO, buf, bytes)
+  }
+  process.onStderr = (buf, bytes) => {
+    just.net.write(just.sys.STDERR_FILENO, buf, bytes)
+  }
+  const status = await watch(process)
+  just.print(status)
+/*
   const snapshot = {
     snapshot_path: './snapshot_file',
     mem_file_path: './mem_file',
     enable_diff_snapshots: false,
     resume_vm: true
   }
-  let res = await fire.put('/snapshot/load', snapshot)
+  res = await fire.put('/snapshot/load', snapshot)
   just.print(JSON.stringify(res, null, '  '))
-
   res = await fire.patch('/vm', { state: 'Paused' })
   just.print(JSON.stringify(res, null, '  '))
 
@@ -25,6 +62,7 @@ async function main (sockName = './busy.sock') {
 
   res = await fire.patch('/vm', { state: 'Resumed' })
   just.print(JSON.stringify(res, null, '  '))
+*/
 }
 
-main(...just.args.slice(2)).catch(err => just.error(err.stack))
+main().catch(err => just.error(err.stack))
