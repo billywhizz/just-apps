f0f75dde731bc3ddebce5b639040c5c2af322e54
Andrew Johnston <apjohnsto@gmail.com>
2021-02-12 12:58:38 +0000

    tweak to sqlite demo

--- sqlite/multi.js
+++ sqlite/multi.js
@@ -6,20 +6,14 @@ just.print(`SQLite version ${version()}`)
 
 function test (rows = 1000) {
   const db = open(':memory:')
-  let stmt = prepare(db, 'DROP TABLE IF EXISTS world').stmt
-  step(stmt)
-  finalize(stmt)
-
-  stmt = prepare(db, `
+  let stmt = prepare(db, `
   CREATE TABLE world (
     id int NOT NULL PRIMARY KEY,
     random int
   )`).stmt
   step(stmt)
   finalize(stmt)
-
   stmt = prepare(db, 'INSERT INTO world (id, random) VALUES ($id, $random)').stmt
-
   const start = Date.now()
   for (let i = 1; i < rows; i++) {
     bindInt(stmt, 1, i)
