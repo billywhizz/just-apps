9a3ed4669ef903c37cbae06ef947a4cebb045b86
Andrew Johnston <apjohnsto@gmail.com>
2021-02-12 12:56:58 +0000

    300k inserts per second for sqlite/ffi

--- sqlite/multi.js
+++ sqlite/multi.js
@@ -3,9 +3,9 @@ const { open, close, version, prepare, step, finalize, reset } = sql
 const { bindInt } = sql
 
 just.print(`SQLite version ${version()}`)
-const db = open(':memory:')
 
 function test (rows = 1000) {
+  const db = open(':memory:')
   let stmt = prepare(db, 'DROP TABLE IF EXISTS world').stmt
   step(stmt)
   finalize(stmt)
@@ -19,28 +19,27 @@ function test (rows = 1000) {
   finalize(stmt)
 
   stmt = prepare(db, 'INSERT INTO world (id, random) VALUES ($id, $random)').stmt
+
   const start = Date.now()
   for (let i = 1; i < rows; i++) {
     bindInt(stmt, 1, i)
-    bindInt(stmt, 2, 0)
+    bindInt(stmt, 2, 1000)
     step(stmt)
     reset(stmt)
   }
   finalize(stmt)
-  just.print(Date.now() - start)
+  const elapsed = Date.now() - start
+  just.print(`rows/sec ${Math.floor(rows / (elapsed / 1000))} rss ${just.memoryUsage().rss}`)
+  close(db)
 }
 
+test(10)
 test(100)
 test(1000)
 test(10000)
 test(100000)
-test(1000000)
-just.print(just.memoryUsage().rss)
-test(1000000)
-just.print(just.memoryUsage().rss)
-test(1000000)
-just.print(just.memoryUsage().rss)
-test(1000000)
-just.print(just.memoryUsage().rss)
-
-close(db)
+function next () {
+  test(1000000)
+  just.setTimeout(next, 100)
+}
+next()
